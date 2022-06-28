ffabc91c17092299736143505b461cc2e59786de
Victor <vx_zz@hotmail.com>
2022-06-28 12:00:17 -0400

    commit tree 185fd1273d94fde85a4003cab823229a3ab192e2

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
-# Your code goes here!
+class Anagram
+
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(anagram)
+        results = []
+        anagram.map do |entry|
+            if (entry.chars.sort == @word.chars.sort)
+                results << entry
+            end
+        end
+        return results
+    end
+
+end
